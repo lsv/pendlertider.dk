acb417455473d426dbc35cc09955261a354d43cb
Martin Aarhof <martin.aarhof@gmail.com>
2021-03-24 16:01:45 +0100

    fixing eslint

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,9 +1,7 @@
 // eslint-disable-next-line import/no-mutable-exports
 let $auth: any
 
-export function initialize(auth) {
-  // eslint-disable-next-line no-console
-  console.log('u', auth)
+export function initialize(auth: any) {
   $auth = auth
 }
 
